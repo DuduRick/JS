291adf4ccafc0e3f4c3fb9741560517c696a2a5f
DuduRick <duduznx.op@gmail.com>
2024-05-31 01:49:40 -0300

    .

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -3,8 +3,17 @@ function add(){
     var number = Number(n.value)
     var select = document.querySelector('select#iselect')
 
-    if ( number < 1 || number > 100){
+    if ( number < 1 || number > 100 ){
         window.alert('Valor inválido ou já encontrado na lista')
     }
+    else{
 
+    var itens = document.createElement('option')
+    itens.text = `Valor ${number} adicionado`
+    select.appendChild(itens)
+    }
+
+}
+function finish(){
+    
 }
