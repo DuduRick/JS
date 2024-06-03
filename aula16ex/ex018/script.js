007315bfc4a7b979cb313a2aca6a22005863107f
DuduRick <duduznx.op@gmail.com>
2024-06-02 23:59:29 -0300

    .

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,17 +1,19 @@
 function add(){
-    var n = document.querySelector('input#inumber')
-    var number = Number(n.value)
-    var select = document.querySelector('select#iselect')
+    let n = document.querySelector('input#inumber')
+    let number = Number(n.value)
+    let select = document.querySelector('select#iselect')
 
     if ( number < 1 || number > 100 ){
         window.alert('Valor inválido ou já encontrado na lista')
     }
     else{
 
-    var itens = document.createElement('option')
+    let itens = document.createElement('option')
     itens.text = `Valor ${number} adicionado`
     select.appendChild(itens)
     }
+
+    let list = []
 
 }
 function finish(){
